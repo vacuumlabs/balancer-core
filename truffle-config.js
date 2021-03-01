3f1cda51207d737fe2c8a1a11460b5497832ee34
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2021-03-01 15:01:10 +0100

    Enable solc optimisation
    Otherwise contract deployment fails with `CreateContractLimit` error.

--- truffle-config.js
+++ truffle-config.js
@@ -61,6 +61,14 @@ module.exports = {
   compilers: {
     solc: {
       version: '0.5.12',
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 100,
+        },
+        evmVersion: 'byzantium',
+      },
     },
   },
 };
